aac0f41379c03f8021d9b6d194b058c32097a032
kHaLiL <khalil_simo@hotmail.fr>
2014-12-11 15:44:43 +0100

    dev

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'travis',
+			'username'  => 'admin',
+			'password'  => 'admin',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
