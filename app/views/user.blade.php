557389331e9c16dd62d3422d59567581d60b843b
kHaLiL <khalil_simo@hotmail.fr>
2015-01-06 22:30:44 +0100

    dev

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -114,7 +114,26 @@ Users
     </div>
 </div>
 
-<div id="ActivitesUsers" style="min-width: 310px; height: 800px;"></div>
+<div class="row-fluid">
+    <div class="box black span12" ontablet="span6" ondesktop="span12">
+        <div class="box-header">
+            <h2><i class="halflings-icon white list"></i><span class="break"></span>Activités</h2>
+
+            <div class="box-icon">
+                <a href="#" class="btn-minimize"><i class="halflings-icon white chevron-up"></i></a>
+                <a href="#" class="btn-close"><i class="halflings-icon white remove"></i></a>
+            </div>
+        </div>
+        <div class="box-content">
+            <div class="alert alert-info">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>INFO!</strong> Vous pouvez zoomer sur ce graphique en séléctionnat une plage verticale.<br />
+                Vous pouvez également séléctionner les éléments à afficher/masquer en cliquent sur les labels dans la légende.
+            </div>
+            <div id="ActivitesUsers" style="min-width: 310px; height: 800px;"></div>
+        </div>
+    </div>
+</div>
 
 <div class="box-content">
     <table class="table table-striped table-bordered bootstrap-datatable datatable">
